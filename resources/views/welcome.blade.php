44c4c9ff21edf8b08664662a79379b447885973b
Kenneth Quintero <kennethgqv@gmail.com>
2022-12-01 19:47:23 -0700

    Change name of textarea to message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
                             </select>
                         </div>
                         <textarea
-                            name="content"
+                            name="message"
                             placeholder="{{ __('¿Qué quieres decir?') }}"
                             class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                         >{{ old('message') }}</textarea>
