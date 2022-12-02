b5ef39eb14a0ed927ffcf5908a0ac4c5711a7386
Kenneth Quintero <kennethgqv@gmail.com>
2022-12-01 18:11:41 -0700

    Add: Thread creation from
    Adds:
    - section for thread creation
    - form for thread creation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\State;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['states' => State::all()]);
 });
 
 Route::get('/dashboard', function () {
